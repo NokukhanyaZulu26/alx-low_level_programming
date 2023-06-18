6675229e66984a83a762fbe935587611da738063
Nokukhanya Zulu <Khanyazulu7@gmail.com>
2023-06-18 16:09:35 -0700

    Created a 100-print_comb3.c file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,24 +5,24 @@
  */
 int main(void)
 {
-        int a, b;
+	int a, b;
 
-        for (a = 48; a <= 56; a++)
-        {
-                for (b = 49; b <= 57; b++)
-                {
-                        if (b > a)
-                        {
-                                putchar(a);
-                                putchar(b);
-                                if (a != 56 || b != 57)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (a = 48; a <= 56; a++)
+	{
+		for (b = 49; b <= 57; b++)
+		{
+			if (b > a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a !=56 || b != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
