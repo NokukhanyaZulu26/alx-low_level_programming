e0c5505816d1dd69ac06d3e17a1ebc2119bf28a3
Nokukhanya Zulu <Khanyazulu7@gmail.com>
2023-06-26 20:22:28 -0700

    Created a 100-atoi.c file

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,26 +7,34 @@
  */
 int _atoi(char *s)
 {
-	int b = 0;
-	int sign = 1;
-	int num = 0;
+	int k, d, n, len, f, digit;
 
-	while (s[b] != '\0')
+	k = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+	while (s[len] != '\0')
+		len++;
+	while (k < len && f == 0)
 	{
-		if (s[b] == '-')
+		if (s[k] == '-')
+			++d;
+		if (s[k] >= '0' && s[k] <= '9')
 		{
-			sign *= -1;
-		}
-		else if (s[b] >= '0' && s[b] <= '9')
-		{
-			num = num * 10 + (s[b] - '0') * sign;
-
-			if (s[b + 1] < '0' || s[b = 1] > '9')
-			{
+			digit = s[k] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[k + 1] < '0' || s[k + 1] > '9')
 				break;
-			}
+			f = 0;
 		}
-		b++;
+		k++;
 	}
-	return (num);
+	if (f == 0)
+	return (0);
+	return (n);
 }
