3e9a4964f0b1a73d40179da53aeccd346ec7914b
Nokukhanya Zulu <Khanyazulu7@gmail.com>
2023-06-28 12:48:15 -0700

    Created a 6-cap_string.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *str)
 		while (!(str[index] >= 97 && str[index] <= 122))
 			index++;
 		
-		if (str[index - 1] == ' ' ||
+				if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
 				str[index - 1] == ',' ||
@@ -29,11 +29,8 @@ char *cap_string(char *str)
 				str[index - 1] == '{' ||
 				str[index - 1] == '}' ||
 				index == 0)
-			str[index] -= 32;
-		
-		index++;
+					str[index] -= 32;
+				index++;
 	}
-	
 	return (str);
 }
-
