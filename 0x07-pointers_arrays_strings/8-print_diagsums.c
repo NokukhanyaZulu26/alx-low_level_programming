834e60c685f79642c21ba90c616941ffe3b31a63
Nokukhanya Zulu <Khanyazulu7@gmail.com>
2023-07-03 15:33:58 -0700

    Created a8-print_diagsums.c file

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Entry point
+ * @a: integers
+ * @size: input
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int w, sum1, sum2;
+
+	for (w = 0; w < size; w++)
+	{
+		sum1 += a[w];
+		a += size;
+	}
+	a -= size;
+
+	for (w = 0; w < size; w++)
+	{
+		sum2 += a[w];
+		a -= size;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
