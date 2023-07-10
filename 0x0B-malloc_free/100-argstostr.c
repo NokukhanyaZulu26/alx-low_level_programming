7ba58cd908b2c8e15876edc601cbe523fb80bd0a
Nokukhanya Zulu <Khanyazulu7@gmail.com>
2023-07-10 07:45:21 -0700

    Created a 100-argstostr.c file

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - Entry point
+ * @ac: int
+ * @av: pointer
+ * Return: Always 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int a, b, x = 0, v = 0;
+
+	 char *r;
+	 
+	 if (ac == 0 || av == NULL)
+			return (NULL);
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b]; b++)
+			v++;
+	}
+	v += ac;
+	r = malloc(sizeof(char) * v + 1);
+	if (r == NULL)
+		return (NULL);
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b]; b++)
+		{
+			r[x] = av[a][b];
+			x++;
+		}
+		if (r[x] == '\0')
+		{
+			r[x++] = '\n';
+		}
+	}
+	return (r);
+}
